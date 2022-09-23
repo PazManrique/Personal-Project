44c97737dab623aaae262f8c8604c7993f45e149
Paz Manrique <nautilus1985@gmail.com>
2022-09-23 21:32:37 +0200

    fixing the database

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/springbootecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/springbootecommerce/dao/ProductCategoryRepository.java
@@ -1,11 +1,12 @@
 package com.luv2code.springbootecommerce.dao;
 
+import com.luv2code.springbootecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.luv2code.springbootecommerce.entity.ProductCategory;
+
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
-    
 }
+
--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/springbootecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/springbootecommerce/dao/ProductRepository.java
@@ -1,9 +1,9 @@
 package com.luv2code.springbootecommerce.dao;
 
+import com.luv2code.springbootecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.luv2code.springbootecommerce.entity.Product;
+
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    
 }
